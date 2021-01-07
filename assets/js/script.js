244b04452e11e1a08d0e9d28711fbf212f9c0713
DevonMartens <dmmartens91@gmail.com>
2021-01-07 18:52:09 -0500

    variable scope was the issue moved things around and it works

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 var btn = document.getElementById("btn");
 
 //charter types
-var passwordIncludes = "";
+// var passwordIncludes = "";
+// var returnPass = "";
 var upperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseLetter = upperCaseLetter.toLowerCase();
 var numbers = "0123456789";
@@ -14,18 +15,18 @@ btn.addEventListener("click", promptUser);
 
 function promptUser() {
     var charterNumber = prompt("How many characters would you like your password to contain 10 is the minimuim for safe passwords and over 32 is too hard to memorize");
-    //blank string scoped locally to reset each time user is prompted 
-
-   var returnPass = "";
+    //reset
+//     var passwordIncludes = "";
+//    var returnPass = "";
 
    //if password is out of scope lets restart
 if (charterNumber < 10) {
     alert("Please select a higher number. For your security we do not create passwords under 10 characters.");
     return;
 } else if (charterNumber > 32) {
-confirmNumber();
+confirmNumber(charterNumber);
 } else {
-    Nextprompts(passwordIncludes);
+    Nextprompts(charterNumber);
 }
 
 
@@ -33,12 +34,16 @@ confirmNumber();
 
 function confirmNumber() {
     if (confirm("Can you memorize a password this long? Please note we are not responsible for forgotten passwords and advise against this length") == true) {
-Nextprompts();
-    } else
+Nextprompts(charterNumber);
+    } else {
     return;
 }
+}
+function Nextprompts(charterNumber ) {
 
-function Nextprompts(passwordIncludes) {
+var passwordIncludes = "";
+var returnPass = "";
+
 
   var upperAdd = confirm("Click OK if you'd like to have uppercase letters in your password. Note you have to add some characters.");
 
@@ -62,6 +67,28 @@ function Nextprompts(passwordIncludes) {
    if(symAdd) {
      passwordIncludes += symbols;
     }
-var promptUser = promptUser();
-btn.addEventListener("click", promptUser);
+
+
+ // If nothing as added add an alert and sent user back to pick some
+ if (!(upperAdd || lowerAdd || symAdd || numberAdd)) {
+    alert("Error! Must include at least one character type.");
+    Nextprompts();
+  } else {
+    for (var i = 0; i < charterNumber; i++) {
+        returnPass += passwordIncludes.charAt(Math.floor(Math.random() * passwordIncludes.length));
+        console.log(returnPass);
+    } 
+console.log(returnPass);
+    return returnPass;
+
+
 }
+}
+// function generatePass(passwordIncludes, returnPass, charterNumber) {
+//for loop to generate a password
+
+// }
+
+
+// var promptUser = promptUser();
+// btn.addEventListener("click", promptUser);
