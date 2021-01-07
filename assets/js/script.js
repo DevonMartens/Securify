58a66c5351c94a26239705193fef5c8f12cf8465
DevonMartens <dmmartens91@gmail.com>
2021-01-07 18:13:06 -0500

    all prompts are being asked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var btn = document.getElementById("btn");
 
 //charter types
-
+var passwordIncludes = "";
 var upperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseLetter = upperCaseLetter.toLowerCase();
 var numbers = "0123456789";
@@ -15,7 +15,7 @@ btn.addEventListener("click", promptUser);
 function promptUser() {
     var charterNumber = prompt("How many characters would you like your password to contain 10 is the minimuim for safe passwords and over 32 is too hard to memorize");
     //blank string scoped locally to reset each time user is prompted 
-   var passwordIncludes = "";
+
    var returnPass = "";
 
    //if password is out of scope lets restart
@@ -25,7 +25,7 @@ if (charterNumber < 10) {
 } else if (charterNumber > 32) {
 confirmNumber();
 } else {
-    Nextprompts();
+    Nextprompts(passwordIncludes);
 }
 
 
@@ -38,8 +38,30 @@ Nextprompts();
     return;
 }
 
-function Nextprompts() {
-}
-// }
+function Nextprompts(passwordIncludes) {
+
+  var upperAdd = confirm("Click OK if you'd like to have uppercase letters in your password. Note you have to add some characters.");
+
+  if (upperAdd == true) {
+
+      passwordIncludes += upperCaseLetter;
+
+  
+
+  var lowerAdd = confirm("Click OK if you'd like to have lowercase letters in your password."); 
+
+
+  if(lowerAdd)
+  passwordIncludes += lowerCaseLetter;
+   }
+   var numberAdd = confirm("Click OK if you'd like to have numbers in your password."); 
+  if(numberAdd) {
+    passwordIncludes += numbers;
+   }
+   var symAdd = confirm("Click OK if you'd like to have symbols letters in your password."); 
+   if(symAdd) {
+     passwordIncludes += symbols;
+    }
 var promptUser = promptUser();
 btn.addEventListener("click", promptUser);
+}
